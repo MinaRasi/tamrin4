ee4b437aa47174d012e88e641283656dc73f795c
MinaRasi <mrasi7306@gmail.com>
2022-03-15 14:37:44 +0330

    add tamrin

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿int number;
+System.Console.WriteLine("mah ra vared konid");
+number=Convert.ToInt32(Console.ReadLine());
+
+switch(number)
+{
+    case 1:
+    System.Console.WriteLine("january");
+    break;
+
+    case 2:
+    System.Console.WriteLine("februry");
+    break;
+    case 3:
+     System.Console.WriteLine("march");
+    break;
+    case 4:
+     System.Console.WriteLine("April");
+    break;
+    case 5:
+     System.Console.WriteLine("May");
+    break;
+     case 6:
+     System.Console.WriteLine("june");
+    break;
+     case 7:
+     System.Console.WriteLine("july");
+    break;
+     case 8:
+     System.Console.WriteLine("August");
+    break;
+    case 9:
+     System.Console.WriteLine("September");
+    break;
+    case 10:
+     System.Console.WriteLine("October");
+    break;
+    case 11:
+     System.Console.WriteLine("novamber");
+    break;
+    case 12:
+     System.Console.WriteLine("december");
+    break;
+    default:
+    System.Console.WriteLine("erorr");
+    break;
+
+}
